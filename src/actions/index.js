16d17b868e27ebbc911512361a5cee3dfafa8c14
Brian O'Carroll <bocarroll36@gmail.com>
2019-02-28 18:12:18 -0800

    actions for messages, user, and text added

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,15 @@ export const typing = (text) => ({
     text
 });
 export const LOAD_MESSAGES = 'LOAD_MESSAGES';
-export const loadMessages = (mess, index) => ({
+export const loadMessages = (messArray, index) => ({
     type: LOAD_MESSAGES,
-    mess,
+    messArray,
     //not sure about index yet
     index
-})
+});
+export const USER_TYPING = 'USER_TYPING';
+export const userTyping = (currUser) => ({
+    type: USER_TYPING,
+    currUser
+});
+
