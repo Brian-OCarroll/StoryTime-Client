39d780377a2db80e38d9918078cb7d75d4ad7c6b
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-31 13:47:24 -0700

    initial react routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import MainPage from './components/main'
 import LandingPage from './components/landingPage.js'
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-        <LandingPage />
+      
+        <Route exact={true} path="/" component={LandingPage} />
+        <Route   path="/home" component={MainPage} />
+       
       </div>
     );
   }
