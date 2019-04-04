0c9e1b4385425ac105a054fb942d6a78d0009a3e
Brian O'Carroll <bocarroll36@gmail.com>
2019-04-04 16:02:40 -0700

    added router with props passin for socket connection

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, withRouter} from 'react-router-dom';
 import MainPage from './components/main'
-import LandingPage from './components/landingPage.js'
+import LandingPage from './components/landingPage.js';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8080')
 class App extends Component {
+  // constructor() {
+  //   super();
+
+  // }
   render() {
     return (
 
       <div className="App">
       
-        <Route exact={true} path="/" component={LandingPage} />
-        <Route   path="/home" component={MainPage} />
+        <Route exact={true} path="/" render={(props) => <LandingPage {...props} socket={socket} />}/>
+        <Route   path="/home" render={(props) => <MainPage {...props} socket={socket} />} />
        
       </div>
     );
